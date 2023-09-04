799a561b0c5808c4ce44ebc396e7b790fd2c981b
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-09-04 09:45:10 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,619 @@
+var myQuestions = [
+  {
+    question: "1. I have three cars. One is big.  __________ two are small.",
+    answers: {
+      a: 'Other',
+      b: 'The others',
+      c: 'The other',
+      d: 'another'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "2. You can go back to Jeddah. I’ll stay here in Dammam for __________three days.",
+    answers: {
+      a: 'others',
+      b: 'other',
+      c: 'the others',
+      d: 'another'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "3. The man __________ I visited used to teach me English.",
+    answers: {
+      a: 'whose',
+      b: 'to whom',
+      c: 'what',
+      d: 'who'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "4. This is the student __________ .",
+    answers: {
+      a: 'about whom we were talking',
+      b: 'we were talking about whom',
+      c: 'whom we were talking about',
+      d: 'we were talking whom about'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "5. The car __________ I sold you is almost new.",
+    answers: {
+      a: 'where',
+      b: 'that',
+      c: 'whom',
+      d: 'what'
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "6. This is the house __________.",
+    answers: {
+      a: 'where he lives in',
+      b: 'in which he lives',
+      c: 'which he lives',
+      d: 'in that he lives'
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "7.	I don’t know __________.",
+    answers: {
+      a: 'where does he lives',
+      b: 'where he live',
+      c: 'where he lives',
+      d: 'where is living'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "8. Put the __________ over there.  Be careful with it.",
+    answers: {
+      a: 'box',
+      b: 'books',
+      c: 'boxes',
+      d: 'pencils'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "9. Khalid had twenty __________ on his farm. He sold them to the meat shop the other day.",
+    answers: {
+      a: 'sheeps',
+      b: 'ship',
+      c: 'sheep',
+      d: 'ships'
+
+    },
+    correctAnswer: 'c'
+  },
+  
+  {
+    question: "10. When we got to the shopping mall, I immediately went to the shop that sells   __________ clothes.",
+    answers: {
+      a: 'men',
+      b: 'mens',
+      c: "men's",
+      d: "mens'"
+
+    },
+    correctAnswer: 'c'
+  },
+  
+  {
+    question: "11. My __________ son finished his third grade class with honors.",
+    answers: {
+      a: 'ten-year-old',
+      b: 'ten years old',
+      c: 'ten-years-old',
+      d: 'ten-years'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "12. The Chinese __________ a long and interesting history, but Chinese __________ very difficult to learn.",
+    answers: {
+      a: 'have - are',
+      b: 'has - is',
+      c: 'have - is',
+      d: 'has - are'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "13. Each boy and girl __________ to go to school. Learning __________ very important.",
+    answers: {
+      a: 'have - are',
+      b: 'has - is',
+      c: 'have - is',
+      d: 'has - are'
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "14. Each of my __________ here.",
+    answers: {
+      a: 'friend are',
+      b: 'friend is',
+      c: 'friends are',
+      d: 'friends is'
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "15. There__________ any mail for you yesterday, but there __________ some letters for you today.",
+    answers: {
+      a: "wasn't - are",
+      b: "wasn't - is",
+      c: "weren't - are",
+      d: "weren't - is"
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "16. CNN News __________ events that take place around the world every day.",
+    answers: {
+      a: 'reports',
+      b: 'report',
+      c: 'reported',
+      d: 'reporting'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "17. Mathematics __________ easy for him. In fact, all subjects __________ easy for him.",
+    answers: {
+      a: 'are - are',
+      b: 'is - are',
+      c: 'is - is',
+      d: 'are - is'
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "18. A lot of prices __________ reduced. In fact, one of the cars __________sold at half price.",
+    answers: {
+      a: 'has been - are',
+      b: 'have benn - are',
+      c: 'has been - was',
+      d: 'have been  - was'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "19. Some people prefer apples, but __________ prefer oranges.",
+    answers: {
+      a: "another",
+      b: "each other",
+      c: "other",
+      d: 'others'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "20. I’m nearly finished. I just need __________ five minutes.",
+    answers: {
+      a: "another",
+      b: "each other",
+      c: "other",
+      d: 'others'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "21. The people __________ we visited yesterday were very hospitable.",
+    answers: {
+      a: "what",
+      b: "whom",
+      c: "whose",
+      d: "which"
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "22. The meeting __________ he went to was interesting.",
+    answers: {
+      a: "where",
+      b: "whom",
+      c: "whose",
+      d: "which"
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "23. The man __________ opinions I respect is my father.",
+    answers: {
+      a: "who",
+      b: "whom",
+      c: "whose",
+      d: "which"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "24. Do you remember the time __________ we traveled to Paris? You were only four then.",
+    answers: {
+      a: "which",
+      b: "when",
+      c: "whom",
+      d: "where"
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "25. I am using a sentence __________ contains an adjective clause.",
+    answers: {
+      a: "whom",
+      b: "whose",
+      c: "who",
+      d: "which"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "26. The apartment __________ he lives in is located on the corner of Citrus and Lavender streets.",
+    answers: {
+      a: "which",
+      b: "in which",
+      c: "where",
+      d: "when"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "27. I tasted three flavors of ice cream. __________ was very good.",
+    answers: {
+      a: "All of them",
+      b: "Much of them",
+      c: "One of them",
+      d: "Some of them"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "28. Did you see the man __________ car is parked in the middle of the road?",
+    answers: {
+      a: "who",
+      b: "whom",
+      c: "whose",
+      d: "which"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "29. The tall man __________ to my father works at the cement factory.",
+    answers: {
+      a: "that talk",
+      b: "is talking",
+      c: "who are talking",
+      d: "talking"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "30. Do you know __________?",
+    answers: {
+      a: "whose house is that",
+      b: "which house is this",
+      c: "whose house that is",
+      d: "where does he live"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "31. Security wants to know __________ yesterday.",
+    answers: {
+      a: "what time we go home",
+      b: "what time we went home",
+      c: "what time do we go home",
+      d: "what time did go home"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "32. Do you think __________ help our children to do their schoolwork? ",
+    answers: {
+      a: "do we need",
+      b: "we should",
+      c: "should we",
+      d: "can we"
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "33. have never understood what __________",
+    answers: {
+      a: "does a tropical storm cause",
+      b: "cause tropica storms",
+      c: "causes tropical storms",
+      d: "did tropical storms cause"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "34. They don’t know whether __________ this invitation or not",
+    answers: {
+      a: "to accept",
+      b: "accept",
+      c: "accepting",
+      d: "accepted"
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "35. Our supervisor told us that we __________ for a transfer at the moment. There is not enough manpower in the unit, so requests won’t be approved.",
+    answers: {
+      a: "must apply",
+      b: "have to apply",
+      c: "should not apply",
+      d: "haven't applied"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "36. Yasser asked his friend __________ television the night before.",
+    answers: {
+      a: "if he watches",
+      b: "did he watch",
+      c: "if he had watched",
+      d: "had he watched"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "37. The secretary __________ I told you about is a friend of the teacher __________ lives near my apartment.",
+    answers: {
+      a: "that - who",
+      b: "that - which",
+      c: "whom - whose",
+      d: "which - whom"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "38. The police __________ me some information that __________ incorrect. I must check it.",
+    answers: {
+      a: "has given - is",
+      b: "have given - is",
+      c: "have given - are",
+      d: "has given - are"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "39. One of my colleagues said that he __________ to Syria, but he __________ to go there.",
+    answers: {
+      a: "has never been - liked",
+      b: "had never been - would like",
+      c: "would never be - liked",
+      d: "was never - would like"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "40. Faisal needs __________ new information He doesn’t want __________ more similar data for this report.",
+    answers: {
+      a: "some - no",
+      b: "not much - no",
+      c: "some - any",
+      d: "many -any'"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "41. They installed new __________ in Room 110. These will be used for the newly arrived books.",
+    answers: {
+      a: "shelf",
+      b: "shelfs",
+      c: "shelves",
+      d: "shelves'"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "42. __________ meeting will be attended by unit supervisors from the refinery.",
+    answers: {
+      a: "Two days",
+      b: "Today's",
+      c: "Today",
+      d: "Today is"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "43. Khalid’s son is tall for his age. He is only __________",
+    answers: {
+      a: "six-year",
+      b: "six-years-old",
+      c: "six years old",
+      d: "six-years old"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "44. The major roads in Jeddah will be repaired soon where __________so congested.",
+    answers: {
+      a: "the traffics are",
+      b: "the traffic are",
+      c: "the traffics is",
+      d: "the traffic is"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "45. There is __________ to learn in English 4B.",
+    answers: {
+      a: "many vocabularies",
+      b: "much vocabulary",
+      c: "much vocabularies",
+      d: "many vocabulary's"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "46. I saw each of my __________ at the safety picnic last Thursday.",
+    answers: {
+      a: "classmate",
+      b: "classmates's",
+      c: "classmates",
+      d: "classmates'"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "47. This set of exercises __________ too difficult for the trainees. The other exercises we gave last time __________easier.",
+    answers: {
+      a: "Are - was",
+      b: "is - were",
+      c: "Are - were",
+      d: "is - was"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "48. The number of medical cases __________ every week, but a number of them __________ not serious.",
+    answers: {
+      a: "increases - are",
+      b: "increase - are",
+      c: "increase - is",
+      d: "increases - is"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "49. We don’t have any __________ choice, so we’ll go ahead with the activities tonight.",
+    answers: {
+      a: "another",
+      b: "each other",
+      c: "other",
+      d: "others"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "50. The part of the city __________ they moved to is very quiet and safe.",
+    answers: {
+      a: "which",
+      b: "what",
+      c: "in which",
+      d: "when"
+    },
+    correctAnswer: 'a'
+  },
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+  function showQuestions(questions, quizContainer){
+    var output = [];
+    var answers;
+    for(var i=0; i<questions.length; i++){
+      answers = [];
+      for(choices in questions[i].answers){
+
+        answers.push(
+          '<label>'
+            + '<input type="radio" name="question'+i+'" value="'+choices+'">'+ choices + '. '+ questions[i].answers[choices] + 
+            '</label>'
+            +'<br>'
+        );
+
+      }
+      output.push(
+        '<div class="question">'  + questions[i].question + '</div>' + '<div class="answers">'  + answers.join('') + '<br>' + '</div>'
+      );
+    }
+    quizContainer.innerHTML = output.join('');
+
+  }
+
+
+  function showResults(questions, quizContainer, resultsContainer){
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    var userAnswer = '';
+    var numCorrect = 0;
+  
+    for(var i=0; i<questions.length; i++){
+
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      if(userAnswer===questions[i].correctAnswer){
+
+        numCorrect++;
+        answerContainers[i].style.color = 'green';
+      }
+      else{
+        answerContainers[i].style.color = 'red';
+      }
+    }
+    
+   resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length + " or " + (numCorrect/questions.length * 100).toFixed(2) + "%"; 
+   
+  }
+
+
+  showQuestions(questions, quizContainer);
+  
+  submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+  }
+
+}
